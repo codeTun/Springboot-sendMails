83e20c50f272ee223916e17e109afbc96a015ade
codeTun <iheblazhary@gmail.com>
2024-05-31 01:12:33 +0100

    📧Setup sendMails server

---
+++ Innovamarkets-sendMails/src/test/java/com/example/Innovamarkets_sendMails/InnovamarketsSendMailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Innovamarkets_sendMails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InnovamarketsSendMailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
