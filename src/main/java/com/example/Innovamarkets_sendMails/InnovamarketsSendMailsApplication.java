48393d7e3ad08ba50eaf216c85eaf2379b6c53e9
codeTun <iheblazhary@gmail.com>
2024-06-05 14:54:17 +0100

    🚨 Test OK | Cleaned the code

--- src/main/java/com/example/Innovamarkets_sendMails/InnovamarketsSendMailsApplication.java
+++ src/main/java/com/example/Innovamarkets_sendMails/InnovamarketsSendMailsApplication.java
@@ -1,30 +1,13 @@
 package com.example.Innovamarkets_sendMails;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-
-import com.example.Innovamarkets_sendMails.service.SendMailsService;
-
 
 @SpringBootApplication
 public class InnovamarketsSendMailsApplication {
-	@Autowired
-	private SendMailsService sendMailsService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(InnovamarketsSendMailsApplication.class, args);
 	}
-
-	// @EventListener(ApplicationReadyEvent.class)
-	// public void triggerMail()  {
-		
-	// 	sendMailsService.sendMail("iheblazhary@gmail.com","hamzahajmtir2002@gmail.com",
-	// 			"This is email subject",
-	// 			"Hello iheb how are you ?");
-		
-	// }
 
 }
--- src/main/java/com/example/Innovamarkets_sendMails/controller/SendMailsController.java
+++ src/main/java/com/example/Innovamarkets_sendMails/controller/SendMailsController.java
@@ -1,11 +1,11 @@
 package com.example.Innovamarkets_sendMails.controller;
 
 import com.example.Innovamarkets_sendMails.service.SendMailsService;
-import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -14,17 +14,14 @@ public class SendMailsController {
 
     private final SendMailsService sendMailsService;
 
-    @Autowired
     public SendMailsController(SendMailsService sendMailsService) {
         this.sendMailsService = sendMailsService;
     }
 
     @PostMapping("/send")
     public String sendMail(@RequestBody MailRequest mailRequest) {
-        for (String recipient : mailRequest.getTo()) {
-            sendMailsService.sendMail(mailRequest.getFrom(), recipient, mailRequest.getSubject(),
-                    mailRequest.getBody());
-        }
-        return "Mail sent";
+        sendMailsService.sendMail(mailRequest.getFrom(), mailRequest.getTo(), mailRequest.getSubject(),
+                mailRequest.getBody());
+        return "Mail sent to : " + mailRequest.getTo();
     }
 }
