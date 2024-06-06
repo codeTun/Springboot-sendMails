865921848b4d13255edfe6cb7e47c5c7c61ed7a7
codeTun <iheblazhary@gmail.com>
2024-06-07 00:50:45 +0100

    🏆 Fixed the email format to html

--- src/main/java/com/example/Innovamarkets_sendMails/service/SendMailsService.java
+++ src/main/java/com/example/Innovamarkets_sendMails/service/SendMailsService.java
@@ -1,9 +1,11 @@
 package com.example.Innovamarkets_sendMails.service;
 
-import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import java.util.List;
 
 @Service
@@ -13,12 +15,20 @@ public class SendMailsService {
     private JavaMailSender mailSender;
 
     public void sendMail(String from, List<String> to, String subject, String body) {
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setFrom(from);
-        message.setTo(to.toArray(new String[0])); // convert list to array
-        message.setSubject(subject);
-        message.setText(body);
-        mailSender.send(message);
+        MimeMessage message = mailSender.createMimeMessage();
+        MimeMessageHelper helper;
+
+        try {
+            helper = new MimeMessageHelper(message, true); // true indicates multipart message
+            helper.setFrom(from);
+            helper.setTo(to.toArray(new String[0])); // convert list to array
+            helper.setSubject(subject);
+            helper.setText(body, true); // true indicates body is HTML
+            mailSender.send(message);
+        } catch (MessagingException e) {
+            System.out.println("Error while sending mail ..");
+            e.printStackTrace();
+        }
 
         System.out.println("Mail sent to: " + to);
     }
