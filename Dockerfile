b79be15bb0cf44a2ffcb30a3a53a0ed27711d56d
codeTun <iheblazhary@gmail.com>
2024-06-17 18:55:39 +0100

    🐳 Docker preparations

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,12 @@ COPY target/*.jar mail-service.jar
 
 EXPOSE 8085
 
+ENV MAIL_HOST=smtp.gmail.com
+ENV MAIL_PORT=587
+ENV MAIL_USERNAME=iheblazhary@gmail.com
+ENV MAIL_PASSWORD=dtpwjacvgseytoek
+
 ENTRYPOINT ["java", "-jar", "mail-service.jar"]
+
+# Build the image
+# docker run -e MAIL_HOST=smtp.gmail.com -e MAIL_PORT=587 -e MAIL_USERNAME=iheblazhary@gmail.com -e MAIL_PASSWORD=dtpwjacvgseytoek -p 8085:8085 -d your_image
--- src/main/java/com/example/Innovamarkets_sendMails/controller/MailRequest.java
+++ src/main/java/com/example/Innovamarkets_sendMails/controller/MailRequest.java
@@ -1,13 +1,42 @@
 package com.example.Innovamarkets_sendMails.controller;
 
-import lombok.Data;
 import java.util.List;
 
-@Data
 public class MailRequest {
     private String from;
     private List<String> to;
     private String subject;
     private String body;
 
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public List<String> getTo() {
+        return to;
+    }
+
+    public void setTo(List<String> to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
 }
